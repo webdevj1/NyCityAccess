189497f1fdf51216b4837cc0f6e50c8e5eca5c2a
Jullie <blema90.jl@gmail.com>
2018-03-03 15:06:59 -0500

    committing selects

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Link, Switch, Route} from 'react-router-dom';
+import  AllCourts  from './AllCourts';
+
+const Home = () =>{
+    return (
+        <div>
+        <header >
+          <h1 className="App-title">Welcome to NYC HOOPS</h1>
+        </header>
+        <Link to="/allcourts" >Find Basket Ball Courts </Link>
+        <Switch>
+            <Route exact path="/allcourts" component={AllCourts} />
+        </Switch>
+        
+        </div>
+    )
+}
+
+export default Home;
